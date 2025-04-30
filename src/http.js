982c439061dd988796d702158c56d171aa96243b
Andrea Huang <huangandrea@hotmail.it>
2025-04-30 21:07:11 +0200

    Fetching Data with useEffect to initialize page

--- src/http.js
+++ src/http.js
@@ -29,3 +29,15 @@ export async function updateUserPlaces(places) {
 
     return resData.message;
 }
+
+export async function fetchUserPlaces() {
+
+    const response = await fetch("http://localhost:3000/user-placess");
+    const resData = await response.json();
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch user places");
+    }
+
+    return resData.places;
+}
