2942e170c0fdce61b674783feb017293e4a81afd
Andrea Huang <huangandrea@hotmail.it>
2025-05-01 07:02:24 +0200

    Custom Hook: Managing State & Returning State Values

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useCallback, useEffect } from "react";
+import { useRef, useState, useCallback } from "react";
 
 import Places from "./components/Places.jsx";
 import Modal from "./components/Modal.jsx";
@@ -7,35 +7,19 @@ import logoImg from "./assets/logo.png";
 import AvailablePlaces from "./components/AvailablePlaces.jsx";
 import { updateUserPlaces, fetchUserPlaces } from "./http.js";
 import ErrorPage from "./components/Error.jsx";
+import { useFetch } from "./hooks/useFetch.js";
 
 function App() {
   const selectedPlace = useRef();
 
-  const [userPlaces, setUserPlaces] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState();
+
 
   const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState();
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-      try {
-        const places = await fetchUserPlaces();
-        setUserPlaces(places);
-      } catch (error) {
-        setError({
-          message:
-            error.message || "Could not fetch places, please try again later",
-        });
-      }
-      setIsFetching(false);
-    }
-
-    fetchPlaces();
-  }, []);
+  const {isFetching, error, fetchedData: userPlaces} = useFetch(fetchUserPlaces, []);
+  
   function handleStartRemovePlace(place) {
     setModalIsOpen(true);
     selectedPlace.current = place;
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+export function useFetch(fetchFunction, initialData) {
+
+    const [isFetching, setIsFetching] = useState(false);
+    const [error, setError] = useState();
+    const [fetchedData, setFetchedData] = useState(initialData);
+
+    useEffect(() => {
+        async function fetchData() {
+            setIsFetching(true);
+            try {
+                const data = await fetchFunction();
+                setFetchedData(data);
+            } catch (error) {
+                setError({
+                    message:
+                        error.message || "Could not fetch data, please try again later",
+                });
+            }
+            setIsFetching(false);
+        }
+
+        fetchData();
+    }, [fetchFunction]);
+
+    return {
+        isFetching,
+        error,
+        fetchedData,
+    };
+}
